bf96114b32ee534f4ca394c9f88417284556e363
Lance Arsenault (cube) <lanceman@vt.edu>
2016-03-13 12:11:36 -0400

    better scripting form

--- installer/install.bash
+++ installer/install.bash
@@ -7,10 +7,6 @@
 scriptdir="$(dirname ${BASH_SOURCE[0]})" || exit $?
 cd "$scriptdir" || exit $?
 scriptdir="$PWD" # now we have full path
-cd .. || exit $?
-
-
-progs[0]=
 
 function Fail()
 {
@@ -22,40 +18,6 @@ function Fail()
 }
 
 
-function InstallPackages()
-{
-    [ -n "$1" ] || Fail "Usage: ${FUNCNAME[0]} LIST_FILE"
-    local i
-
-    # keep the pipe line program in the same shell context
-    # so that progs[] and i get set in this shell context
-    shopt -s lastpipe
-
-    grep -v -e '^ *#\|^ *$' "$1" |\
-        while read -r line ; do
-            progs[$i]="$line"
-            let i=$i+1
-        done
-    progs[$i]=
-    i=0
-    while [ -n "${progs[$i]}" ] ; do
-        echo "Running: ${progs[$i]}"
-        if ! ${progs[$i]} ; then
-            Fail "Running: ${progs[$i]} failed"
-        fi
-        let i=$i+1
-    done
-
-    echo
-    echo "----------- SUCCESSFULLY INSTALLED --------------"
-    echo
-    cat $1
-    echo
-    echo "-------------- SUCCESS --------------------------"
-    echo
-}
-
-
 host="$(hostname -s)" || exit $?
 [ -n "$host" ] || exit 1
 hostfile="$scriptdir/$host"
@@ -64,10 +26,15 @@ hostfile="$scriptdir/$host"
 \n\
   Copy an existing one from $scriptdir to $hostfile and edit it."
 
-
+set +x
 echo
 echo "Running install scripts from $hostfile"
 echo
 
-InstallPackages "$hostfile"
+source "$hostfile"
+
+set +x
+echo
+echo "-------------- SUCCESS --------------------------"
+echo
 
