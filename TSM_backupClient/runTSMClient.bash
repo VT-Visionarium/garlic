f2c9e70127c6ffb76ac790770a4c42ec7e8f7fd5
Lance Arsenault <lanceman@vt.edu>
2016-04-14 13:13:15 -0400

    minor

--- TSM_backupClient/runTSMClient.bash
+++ TSM_backupClient/runTSMClient.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Seem to be a generic client interface to the TSM server
-# a file backup and restore system.
+# dsmc seems to be a generic client interface to the TSM server, a file
+# backup and restore system.
 
 exec /opt/tivoli/tsm/client/ba/bin/dsmc
