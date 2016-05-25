447827095181043f94881c70f6867e2c389e90fc
Lance Arsenault <lanceman@vt.edu>
2016-05-25 17:54:59 -0400

    added VMD with freevr

---
+++ freevr/freevr_0.6f/install.bash
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+scriptdir="$(dirname ${BASH_SOURCE[0]})" || exit $?
+cd "$scriptdir" || exit $?
+scriptdir="$PWD" # now we have full path
+source ../../common.bash
+
+# The source to VMD can only be downloaded interactively
+# See ../README
+
+TAR=$topsrcdir/freevr_0.6f.tar.gz
+URL="http://www.freevr.org/Downloads/freevr_0.6f.tar.gz"
+
+T_TAR=$topsrcdir/fvtutogl_vr2011.tar.gz
+T_URL="http://freevr.org/Downloads/fvtutogl_vr2011.tar.gz"
+
+if [ ! -f $TAR ] ; then
+    set -x
+    wget $URL -O $TAR || Fail
+    [ -f $TAR ] || Fail
+fi
+
+if [ ! -f $T_TAR ] ; then
+    set -x
+    wget $T_URL -O $T_TAR || Fail
+    [ -f $T_TAR ] || Fail
+fi
+
+
+
+
+######################################################################
+
+builddir=
+# create a new build_03/ or build_04/ or so on.
+MkBuildDir builddir
+cd "$builddir" || Fail
+
+set -x
+
+tar -xzf "$TAR" || Fail
+
+cd $name/src || Fail
+
+tab="$(echo -en '\t')"
+
+cat << EOF >> Makefile || Fail
+install:
+${tab}mkdir -p $prefix/lib $prefix/include
+${tab}cp libfr*.a freevr.h vr_*.h $prefix/lib
+${tab}cp freevr.h vr_*.h $prefix/include
+
+EOF
+
+make linux2.6-glx-64 || Fail
+make install || Fail
+
+
+tutdir=$prefix/tutorial
+mkdir -p $tutdir || Fail
+cd $tutdir || Fail
+tar -xzf $T_TAR || Fail
+
+cd $tutdir/FVtut_ogl/Code || Fail
+# patch file made with:
+# diff originalfile updatedfile > file.patch
+mv Makefile Makefile.org || Fail
+patch Makefile.org\
+ -i $scriptdir/tutorial_Makefile.patch\
+ -o Makefile || Fail
+
+PrintSuccess
--- vmd/vmd-1.9.2/install.bash
+++ vmd/vmd-1.9.2/install.bash
@@ -9,6 +9,8 @@ source ../../common.bash
 # See ../README
 
 TAR=$topsrcdir/vmd-1.9.2.src.tar.gz
+tutortarfile=$topsrcdir/vmd-tutorial-files.tar.gz
+tutorURL="http://www.ks.uiuc.edu/Training/Tutorials/vmd/vmd-tutorial-files.tar.gz"
 #name=vmd-1.9.2 # from ../../common.bash
 
 if [ ! -f $TAR ] ; then
@@ -21,7 +23,13 @@ if [ ! -f $TAR ] ; then
     Fail
 fi
 
-#########################################################################
+if [ ! -f $tutortarfile ] ; then
+    set -x
+    wget $tutorURL -O $tutortarfile || Fail
+    [ -f $tutortarfile ] || Fail
+fi
+
+######################################################################
 
 builddir=
 # create a new build_03/ or build_04/ or so on.
@@ -33,12 +41,27 @@ set -x
 tar -xzf "$TAR" || Fail
 
 export PLUGINDIR="$prefix/plugins"
+export TCLINC=-I/usr/include/tcl8.6
+export TCLLIB=/usr/lib/x86_64-linux-gnu/libtcl.so
+export TKINC=$TCLINC
+export TKLIB=/usr/lib/x86_64-linux-gnu/libtk.so
 
 cd "$builddir"/plugins || Fail
+
+# We needed to debug this build so silent needed
+# to be removed.
+for m in Makefile */Makefile ; do
+    sed -e\
+ 's/^\.SILENT\:.*/# lance removed .SILENT here/g'\
+ $m > $m.newZ || Fail
+    mv $m.newZ $m || Fail
+done
+
 # parallel make fails
 #make -j$ncores LINUXAMD64 || Fail
 make LINUXAMD64 || Fail
-make -j3 distrib || Fail
+make distrib || Fail
+
 
 cd "$builddir"/$name || Fail
 
@@ -58,21 +81,34 @@ ln -s "$PLUGINDIR" .
 # emailing vmd@ks.uiuc.edu.  The web site has "VMD Community Pages" as
 # a blocked page which requires a name and password.
 
+# graphic/windowing library:
+# I'd guess you'd pick some of these and some combin
+# OPENGL
+# OPENGLPBUFFER
+# SDL
+# FLTKOPENGL
+# MESA
+# CAVE # CAVE libs from VRCO
+# FREEVR
+
 ./configure\
  LINUXAMD64\
  OPENGL\
  GCC\
  TCL\
+ FLTK\
  TK\
+ FREEVR\
  PTHREADS\
  || Fail
 
 cd src || Fail
-make || Fail
+# parallel make fails
 #make -j$ncores || Fail
+make || Fail
 make -j3 install || Fail
 
-#ANOTHER BUG FIX:  remove broken rlwrap options from wrapper script
+#ANOTHER BUG FIX: remove broken rlwrap options from wrapper script
 # http://www.ks.uiuc.edu/Research/vmd/mailing_list/vmd-l/19158.html
 # Given how old the bug is I'd say that they don't give a shit.
 sed -e 's/\-b(){}\[\],&\^\%#\;|\\\\//g'\
@@ -80,5 +116,12 @@ sed -e 's/\-b(){}\[\],&\^\%#\;|\\\\//g'\
 chmod 755 $prefix/bin/vmd || Fail
 cp $prefix/bin/vmd $prefix/bin/$name || Fail
 
+set +x
+tprefix="$prefix/tutorial"
+echo "Installing tutorial files in $tprefix"
+set -x
+mkdir -p "$tprefix" || Fail
+cd "$tprefix" || Fail
+tar -xzf "$tutortarfile" || Fail
 
 PrintSuccess
