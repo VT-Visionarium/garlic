0b64c0a7bf631d996f1d9995f31a206681077620
Lance Arsenault <lanceman@vt.edu>
2016-06-20 09:41:51 -0400

    worked on Unity install

--- Unity/post_install.bash
+++ Unity/post_install.bash
@@ -4,11 +4,14 @@ scriptdir=$(dirname $0) || exit $?
 cd $scriptdir || exit $?
 
 prefix=/usr/local/encap/unity-editor-5.4.0b18+20160524
+unity=$prefix/bin/Unity
 
 set -x
 
 mkdir -p $prefix/bin || exit 1
-cp Unity $prefix/bin || exit 1
+echo "#!/bin/bash" > $unity || exit 1
+echo "# This is a generated file" >> $unity || exit 1
+cat Unity README >> $unity || exit 1
 
 set +x
 ls --color=auto -R $prefix
