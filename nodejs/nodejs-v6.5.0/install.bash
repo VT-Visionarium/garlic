9cc8dc89ed07b4005e2a9d9f03df640e4a090f00
Lance Arsenault <lanceman@vt.edu>
2016-08-29 17:29:16 -0400

    a newer nodejs

---
+++ nodejs/nodejs-v6.5.0/install.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+scriptdir="$(dirname ${BASH_SOURCE[0]})" || exit $?
+cd "$scriptdir" || exit $?
+scriptdir="$PWD" # now we have full path
+source ../../common.bash
+    
+GitCreateClone https://github.com/nodejs/node.git
+
+GitToBuildDir "${name##nodejs-}"
+#GitToBuildDir v6.5.0 # tag v6.5.0 is not same as $name
+
+
+
+# This is impressive; this built on the first try
+# with this simple bash script.
+
+# We are now in the newly created build dir
+./configure\
+ --prefix=$prefix\
+ || Fail
+
+make -j$ncores || Fail
+make -j3 install || Fail
+
+PrintSuccess
