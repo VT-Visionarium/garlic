60c4320162e84434685347df33274887b65e67b6
Lance Arsenault <lanceman@vt.edu>
2017-05-26 09:58:42 -0400

    added xbuntu 16.04 /etc/skel/

---
+++ etc_skel/xubuntu-16.04/root_install.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# this script does not do much, just copies
+# some files into /etc/skel/
+
+function Fail()
+{
+    echo -e "\n$*\n"
+    exit 1
+}
+
+
+# run as root or not at all
+[ "$(id -u)" = 0 ] || Fail "You must run this as root"
+
+set -x
+scriptdir="$(dirname ${BASH_SOURCE[0]})" || exit $?
+cd "$scriptdir" || exit $?
+scriptdir="$PWD"
+
+
+for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ; do
+    if [ ! -d /root/ORG_preSKEL_$i ] ; then
+        bakdir=/root/ORG_preSKEL_$i
+        break
+    fi
+done
+
+[ -n "$bakdir" ] || Fail "Too many backup dirs"
+
+mkdir $bakdir || exit $?
+cp -a /etc/skel $bakdir || exit $?
+
+cp -a skel/.??* /etc/skel/ || exit $?
+chmod -R a+r /etc/skel || exit $?
+set +x
+
+
+if [ -d "$host_skel" ] ; then
+    for i in $host_skel/.??* $host_skel/* ; do
+        if [ -e "$i" ] ; then
+            echo "cp -r $i /etc/skel"
+            cp -r $i /etc/skel || Fail "cp -r $i /etc/skel FAILED"
+        fi
+    done
+fi
+
+echo "SUCCESS"
